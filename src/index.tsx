92dd9a9bb456e666f67d5914eba2acf91abccade
Vut Pov <vuthpov26@gmail.com>
2020-07-22 10:25:44 +0700

    add husky

--- src/index.tsx
+++ src/index.tsx
@@ -2,52 +2,98 @@ import React, { useCallback } from 'react'
 import styles from './styles.module.css'
 import { useState } from 'react'
 
+type SetComponentState = (values: any) => void
+
 interface LoginUiProps {
-  title?: React.ReactNode
-  submitButton?: (onSubmit: Function) => React.ReactNode
+  renderHeaderComponents?: (
+    setComponentState: SetComponentState
+  ) => React.ReactNode
   logoUrl?: string
   footer?: React.ReactNode
+  onSubmit: (values: any) => void
+  btnSubmitStyle?: object
+  loginCardStyle?: object
+  logoStyle?: object
+  renderFooterComponents?: (
+    setComponentState: SetComponentState
+  ) => React.ReactNode
 }
 
 const LoginUi: React.FC<LoginUiProps> = (props) => {
-  const { title, submitButton, logoUrl, footer } = props
+  const {
+    renderHeaderComponents,
+    renderFooterComponents,
+    logoUrl,
+    footer,
+    logoStyle,
+    btnSubmitStyle,
+    loginCardStyle
+  } = props
 
-  const [errorUsername] = useState('Username required')
-  const [errorPassword] = useState('')
-
-  const onSubmit = useCallback(() => {}, [])
-
-  const renderSubmit = useCallback(() => {
-    if (submitButton) {
-      return submitButton(onSubmit)
-    }
-
-    return (
-      <button className={styles.btnSubmit} onClick={() => onSubmit()}>
-        Login
-      </button>
-    )
-  }, [submitButton, onSubmit])
+  const [errorUsername, setErrorUsername] = useState('')
+  const [errorPassword, setErrorPassword] = useState('')
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    remember: false
+  })
 
   const renderLogo = useCallback(() => {
     const logoToShow = logoUrl || ''
     return (
       <div className={styles.logoContainer}>
-        <img src={logoToShow} alt={""}/>
+        <img
+          src={logoToShow}
+          alt={''}
+          style={logoStyle}
+          className={styles.logo}
+        />
       </div>
     )
   }, [logoUrl])
 
-  return (
-    <div className={styles.container}>
-      {renderLogo()}
-      <div className={styles.loginCard}>
-        {title}
+  const handleChange = useCallback(
+    ({ name, value }) => {
+      setValues((old) => {
+        return {
+          ...old,
+          [name]: value
+        }
+      })
+    },
+    [values]
+  )
 
+  const onSubmit = useCallback(() => {
+    const setErrorMessage = (valueToCheck: string, errorMessage: string, setErrorFunction: Function) => {
+      if (valueToCheck === '') {
+        setErrorFunction(errorMessage)
+      } else {
+        setErrorFunction('')
+      }
+    }
+
+    setErrorMessage(values.username, 'Username required', setErrorUsername)
+    setErrorMessage(values.password, 'Password required', setErrorPassword)
+
+    if (values.username !== '' && values.password !== '') {
+      props.onSubmit(values)
+    }
+  }, [values])
+
+  return (
+    <div style={{background: '#fafafa'}}>
+      <div className={styles.container}>
+      {renderLogo()}
+      <div className={styles.loginCard} style={loginCardStyle}>
+        {renderHeaderComponents && renderHeaderComponents(setValues)}
         <div className={styles.controlContainer}>
           <input
             name={'username'}
             type={'text'}
+            onChange={(e) => {
+              handleChange({ name: e.target.name, value: e.target.value })
+            }}
             className={styles.customInput}
             placeholder={'Username'}
           />
@@ -58,22 +104,44 @@ const LoginUi: React.FC<LoginUiProps> = (props) => {
           <input
             name={'password'}
             type={'password'}
+            onChange={(e) => {
+              handleChange({ name: e.target.name, value: e.target.value })
+            }}
             className={styles.customInput}
             placeholder={'Password'}
           />
           <span className={styles.errorText}>{errorPassword}</span>
         </div>
 
+        {renderFooterComponents && renderFooterComponents(setValues)}
+
         <div className={styles.rememberContainer}>
-          <input name={'remember'} type={'checkbox'} id={'remember'} />
+          <input
+            name={'remember'}
+            type={'checkbox'}
+            id={'remember'}
+            onChange={(e) => {
+              handleChange({ name: e.target.name, value: e.target.checked })
+            }}
+          />
           <label htmlFor={'remember'} className={styles.rememberlabel}>
             Remember me
           </label>
         </div>
-        {renderSubmit()}
+
+        <button
+          className={styles.btnSubmit}
+          style={btnSubmitStyle}
+          onClick={() => {
+            onSubmit()
+          }}
+        >
+          Login
+        </button>
       </div>
 
       {footer}
+    </div>
     </div>
   )
 }
